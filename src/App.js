8e37e6e8e2046ac04984ae896eb87267430456b9
Basit <Basit@>
2023-06-16 11:54:35 +0530

    basic features

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Button } from '@chakra-ui/react';
+import React from 'react';
+import {BrowserRouter as Router,Route,Routes} from "react-router-dom"
+
+function App() {
+  return (
+ <Router>
+  <Routes>
+    <Route/>
+  </Routes>
+ </Router>
+  )
+
+}
+
+export default App;
